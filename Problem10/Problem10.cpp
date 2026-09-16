cc02b935420c38b78f3fc1fd739d7ecd835ac062
mohamed-alhussin <mohamedtahsinalhosen@gmail.com>
2026-09-16 16:28:49 +0300

    Add Problem 10 solution :

---
+++ Problem10/Problem10.cpp
@@ -0,0 +1,40 @@
+//Problem #10:Print Digits In Order ,Write a porogram to read a number and print it in order from left to right..
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<cstdlib>
+using namespace std;
+int ReadPositiveNumber(string Message) {
+	int Number = 0;
+	do
+	{
+		cout << Message <<endl;
+		cin >> Number;
+
+	} while (Number <= 0);
+	return Number;
+}
+int ReverseNumber(int Number) {
+	int Remendir = 0, Reverse = 0;
+	while (Number > 0)
+	{
+		Remendir = Number % 10;
+		Number = Number / 10;
+		Reverse = Reverse * 10 + Remendir;
+	}
+	return Reverse;
+}
+void PrintDigits(int Number) {
+	int Remendir = 0;
+	while (Number > 0)
+	{
+		Remendir = Number % 10;
+		Number = Number / 10;
+		cout << Remendir << endl;
+
+	}
+}
+int main() {
+	PrintDigits(ReverseNumber(ReadPositiveNumber("Please enter positive number ?")));
+	return 0;
+}
