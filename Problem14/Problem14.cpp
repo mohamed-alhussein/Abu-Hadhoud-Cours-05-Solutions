2584bca937783bf9938db768280a425975e9cf6f
mohamed-alhussin <mohamedtahsinalhosen@gmail.com>
2026-09-17 12:38:18 +0300

    Add Problem 14 solution :

---
+++ Problem14/Problem14.cpp
@@ -0,0 +1,31 @@
+//Problem #14:Inverted Letter Pattern ,Write a program to read a number  and print inverted letter pattern as follows..
+// 65...90
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<cstdlib>
+using namespace std;
+int ReadPositiveNumber(string message) {
+	int Number = 0;
+	do
+	{
+		cout << message << endl;
+		cin >> Number;
+	} while (Number <= 0);
+	return Number;
+}
+
+void PrintInvertedLetterPattern(int Number) {
+	for (int i =65+ Number-1; i >= 65; i--)
+	{
+		for (int j =1 ; j <=Number-(65+Number-1-i) ; j++)
+		{
+			cout << char(i);
+		}
+		cout << endl;
+	}
+}
+int main() {
+	PrintInvertedLetterPattern(ReadPositiveNumber("Please enter a positive number ?"));
+	return 0;
+}
